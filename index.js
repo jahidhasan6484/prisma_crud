9656eb44db72a998b116b7408eae286b49cd0f8a
jahidhasan6484 <jahid@lintechgroup.co.uk>
2023-12-13 19:07:11 +0600

    start again

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const app = express();
 
 app.use(express.json());
 
-const userRoute = require("./src/user/user.route");
-
-app.use("/api/user", userRoute);
-
 app.listen(5000, () => {
-  console.log("Prisma server running on port 5000");
+  console.log("Prisma server is running on port", 5000);
 });
--- src/user/user.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { signUp, allUsers } = require("./user.controller");
-const router = express.Router();
-
-router.post("/sign-up", signUp);
-router.get("/all", allUsers);
-
-module.exports = router;
